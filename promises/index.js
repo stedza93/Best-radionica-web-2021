8b2935edcdbd57477aeac3f9cffa8f1a11fe4446
Stefan Miladinovic <stefan@coinis.com>
2020-12-20 12:26:41 +0100

    Kreiran promise i izvrsena funkcija nakon sto je upisan novi student

--- promises/index.js
+++ promises/index.js
@@ -1,4 +1,3 @@
-console.log("hello");
 const students = [
   { name: "Jovan", lastname: "Jovanovic" },
   { name: "Marko", lastname: "Markovic" },
@@ -7,7 +6,7 @@ const students = [
 function listAllStudents() {
   let output = "";
   students.forEach((student, index) => {
-    console.log(student, index);
+    // console.log(student, index);
     output += `<ul>
              <li>Ime: ${student.name}</li>
              <li>Prezime: ${student.lastname}</li>
@@ -16,9 +15,23 @@ function listAllStudents() {
   document.getElementById("studentList").innerHTML = output;
 }
 function addStudent() {
-  setTimeout(function () {
-    students.push({ name: "Ivan", lastname: "Ivanovic" });
-  }, 2000);
+  return new Promise(function (resolve, reject) {
+    const someError = false;
+    setTimeout(function () {
+      students.push({ name: "Ivan", lastname: "Ivanovic" });
+      if (someError) {
+        reject("Desila se greska");
+      } else {
+        resolve("FUNKCIJA JE ZAVRSENA uspjesno");
+      }
+    }, 2000);
+  });
 }
-addStudent();
-listAllStudents();
+addStudent()
+  .then((result) => {
+    console.log(result);
+    listAllStudents();
+  })
+  .catch((error) => {
+    console.log(error);
+  });
