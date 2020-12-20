07bf28378b9c2896713fec8950c66f93ccbaf972
Stefan Miladinovic <stefan@coinis.com>
2020-12-20 12:35:27 +0100

    refactored .then with async function

--- promises/index.js
+++ promises/index.js
@@ -3,14 +3,51 @@ const students = [
   { name: "Marko", lastname: "Markovic" },
 ];
 
+// PROMISES WITH .then & .catch
+
+// function listAllStudents() {
+//   let output = "";
+//   students.forEach((student, index) => {
+//     // console.log(student, index);
+//     output += `<ul>
+//              <li>Ime: ${student.name}</li>
+//              <li>Prezime: ${student.lastname}</li>
+//              </ul>`;
+//   });
+//   document.getElementById("studentList").innerHTML = output;
+// }
+// function addStudent() {
+//   return new Promise(function (resolve, reject) {
+//     const someError = false;
+//     setTimeout(function () {
+//       students.push({ name: "Ivan", lastname: "Ivanovic" });
+//       if (someError) {
+//         reject("Desila se greska");
+//       } else {
+//         resolve("FUNKCIJA JE ZAVRSENA uspjesno");
+//       }
+//     }, 2000);
+//   });
+// }
+// addStudent()
+//   .then((result) => {
+//     console.log(result);
+//     listAllStudents();
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+//PROMISES async/await
+
 function listAllStudents() {
   let output = "";
   students.forEach((student, index) => {
     // console.log(student, index);
     output += `<ul>
-             <li>Ime: ${student.name}</li>
-             <li>Prezime: ${student.lastname}</li>
-             </ul>`;
+               <li>Ime: ${student.name}</li>
+               <li>Prezime: ${student.lastname}</li>
+               </ul>`;
   });
   document.getElementById("studentList").innerHTML = output;
 }
@@ -24,14 +61,13 @@ function addStudent() {
       } else {
         resolve("FUNKCIJA JE ZAVRSENA uspjesno");
       }
-    }, 2000);
+    }, 1000);
   });
 }
-addStudent()
-  .then((result) => {
-    console.log(result);
-    listAllStudents();
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+
+async function waitForStudentAdd() {
+  const results = await Promise.all([addStudent(), addStudent(), addStudent()]);
+  console.log(results);
+  listAllStudents();
+}
+waitForStudentAdd();
