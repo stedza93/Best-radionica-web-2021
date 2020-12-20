2801a6ffb1b63060235021ce1220e27e67f95d49
Stefan Miladinovic <stefan@coinis.com>
2020-12-20 13:09:58 +0100

    fetch rick and morty and list on page

--- fetch/primjer1/index.js
+++ fetch/primjer1/index.js
@@ -10,18 +10,37 @@
 // getText();
 
 // GET JSON
-function getJson() {
-  fetch("./test.json")
+// function getJson() {
+//   fetch("./test.json")
+//     .then((res) => res.json())
+//     .then((res) => {
+//       let output = "";
+//       res.forEach((student, index) => {
+//         output += `<ul>
+//                        <li>Ime: ${student.name}</li>
+//                        <li>Prezime: ${student.lastname}</li>
+//                        </ul>`;
+//       });
+//       document.getElementById("studentList").innerHTML = output;
+//     });
+// }
+// getJson();
+
+function getRickAndMortyChar() {
+  fetch("https://rickandmortyapi.com/api/character")
     .then((res) => res.json())
-    .then((res) => {
-      let output = "";
-      res.forEach((student, index) => {
-        output += `<ul>
-                       <li>Ime: ${student.name}</li>
-                       <li>Prezime: ${student.lastname}</li>
-                       </ul>`;
-      });
-      document.getElementById("studentList").innerHTML = output;
-    });
+    .then((res) => listAllChar(res.results));
 }
-getJson();
+getRickAndMortyChar();
+function listAllChar(chars) {
+  console.log(chars);
+  let output = "";
+  chars.forEach(element => {
+      output+=`<ul>
+      <li><img src=${element.image}></li>
+      <li>Name: ${element.name}</li>
+      <li>Species: ${element.species}</li>
+      </ul>`
+  });
+  document.getElementById("studentList").innerHTML = output;
+}
